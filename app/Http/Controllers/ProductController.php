02c9d22f620ad00ed9f56fd35d5ee656e745b894
peter <peterandrianpriestley@gmail.com>
2024-09-25 20:51:33 +0700

    module product: create and store

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\Supplier;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class ProductController extends Controller
@@ -23,4 +25,63 @@ class ProductController extends Controller
     
         return view('products.index', compact('products'));    
     }
+
+    /* 
+    * create
+    *
+    * @return View
+    */
+
+    public function create() : View
+    {
+        $product = new Product;
+        $supplier = new Supplier;
+
+        $data['categories'] = $product->get_category_product()->get();
+        $data['suppliers'] = $supplier->get_supplier()->get();
+
+        return view('products.create', compact('data'));    
+    }
+
+    /* 
+    * store
+    *
+    * @param mixed $request
+    * @return RedirectedResponse
+    */
+
+    public function store(Request $request) : RedirectResponse
+    {
+        $validatedData = $request->validate([
+            'image'               => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'               => 'required|min:5',
+            'product_category_id' => 'required|integer',
+            'id_supplier'         => 'required|integer',
+            'description'         => 'required|min:10',
+            'price'               => 'required|numeric',
+            'stock'               => 'required|numeric',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $image->store('public/images'); // Simpan gambar ke folder penyimpanan
+        
+            // Create product
+            Product::create([
+                'image'               => $image->hashName(),
+                'title'               => $request->title,
+                'product_category_id' => $request->product_category_id,
+                'id_supplier'         => $request->id_supplier,
+                'description'         => $request->description,
+                'price'               => $request->price,
+                'stock'               => $request->stock,
+            ]);
+        
+            // Redirect to index
+            return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
+        }
+        
+        return redirect()->route('products.index')->with(['error' => 'Failed to upload image.']);
+    }
 }
+
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,14 +4,47 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'image',
+        'title',
+        'product_category_id',
+        'id_supplier',
+        'description',
+        'price',
+        'stock'
+    ];
+    
     public function get_product(){
         $sql = $this->select("products.*", "category_product.product_category_name as product_category_name", "suppliers.supplier_name as supplier_name")
                     ->join('suppliers', 'suppliers.id', '=', 'products.id_supplier')
                     ->join('category_product', 'category_product.id', '=', 'products.product_category_id');
         return $sql;
+    }
+
+    public function get_category_product(){
+        $sql = DB::table('category_product')->select("*");
+
+        return $sql;
+    }
+
+    public $timestamps = true;
+
+    public static function storeProduct($request, $image)
+    {
+        return self::create([
+            'image'               => $image->hashName(),
+            'title'               => $request->title,
+            'product_category_id' => $request->product_category_id,
+            'id_supplier'         => $request->id_supplier,
+            'description'         => $request->description,
+            'price'               => $request->price,
+            'stock'               => $request->stock    
+        ]);
     }
 }
